a95e74b2e0cfaa9cf38a9226721cc7d7605435a3
arfajsheru <arfatsheru74@gmail.com>
2024-09-02 14:46:09 +0530

    add navbar edite

--- cr-restaurant/src/component/Navbar.jsx
+++ cr-restaurant/src/component/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo2 from "../assets/logo2.jpg";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { IoSearchSharp } from "react-icons/io5";
 import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { MdOutlineLocalGroceryStore } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { TiThMenu } from "react-icons/ti";
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center justify-between py-2 font-medium">
       <img src={logo2} className="w-16 border rounded-full" alt="" />
@@ -41,15 +43,30 @@ const Navbar = () => {
           <p className="text-[9px]">Favorite</p>
         </div>
 
-        <div className="flex flex-col items-center">
+        <Link to={"/cart"} className="flex flex-col items-center">
           <MdOutlineLocalGroceryStore className="text-2xl" />
           <p className="text-[9px]">Cart</p>
-        </div>
+        </Link>
         <div className="flex flex-col items-center">
           <CgProfile className="text-2xl" />
           <p className="text-[9px]">Profile</p>
         </div>
+        <div
+          onClick={() => setVisible(true)}
+          className="flex flex-col items-center md:hidden"
+        >
+          <TiThMenu className="text-2xl"  onClick={() => setVisible(true)} />
+          <p className="text-[9px]">Menu</p>
+        </div>
       </div>
+
+      {/* sidebar Menu */}
+
+      <div
+        className={`absolute top-0 right-0 bottom-0 left-0 overflow-hidden transition-all bg-black ${
+          visible ? "w-full h-full z-50" : "w-0 h-0"
+        }`}
+      ></div>
     </div>
   );
 };
