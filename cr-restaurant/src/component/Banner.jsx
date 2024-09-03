a20b5ba8a6ac70f39154a429818cee22ac157a83
arfajsheru <arfatsheru74@gmail.com>
2024-09-03 13:08:13 +0530

    added banner

--- cr-restaurant/src/component/Banner.jsx
+++ cr-restaurant/src/component/Banner.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import banner1 from "../assets/banner1.jpeg";
 import banner2 from "../assets/banner2.jpeg";
 import banner3 from "../assets/banner3.jpeg";
 import { IoStarHalfOutline } from "react-icons/io5";
 
 // Importing Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; // Swiper styles import karein
-import 'swiper/css/navigation'; // Navigation buttons ke styles
-import 'swiper/css/pagination'; // Pagination dots ke styles
-import { Navigation, Pagination, Autoplay } from 'swiper/modules'; // Swiper modules import
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Swiper styles import karein
+import "swiper/css/navigation"; // Navigation buttons ke styles
+import "swiper/css/pagination"; // Pagination dots ke styles
+import { Navigation, Pagination, Autoplay } from "swiper/modules"; // Swiper modules import
 
 const Banner = () => {
   const bannerList = [
@@ -46,35 +46,47 @@ const Banner = () => {
   ];
 
   return (
-    <div className="relative w-full h-[88vh] rounded-md  overflow-hidden">
+    <div className="relative w-full  h-full md:h-[88vh] rounded-md  overflow-hidden">
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         }}
-        // pagination={{ clickable: true }}
-        // autoplay={{ delay: 3000 }} // Auto-slide delay time
-        // loop={true} // Infinite loop slider
-        // spaceBetween={30} // Space between slides
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }} // Auto-slide delay time
+        loop={true} // Infinite loop slider
+        spaceBetween={30} // Space between slides
       >
         {bannerList.map((banner) => (
+        
           <SwiperSlide key={banner._id}>
-            <div className='relative w-full h-full '>
+            <div className="relative w-full h-[60vh] md:h-full ">
               <img
                 src={banner.imgUrl}
                 alt=""
-                className='object-cover brightness-50  w-full h-[88vh] rounded-md'
+                className="object-cover brightness-50  w-full h-full md:h-[88vh] rounded-md"
               />
-              <div className='w-[75%] absolute top-[35%] left-4  text-white'>
-                  <p className='text-xl font-bold'>{banner.title}</p>
-                  <p className=''>{banner.title2}</p>
-                  <p className='hidden '>{banner.dec}</p>
-                  <div className='flex items-center gap-3'>
-                    <p className='flex items-center gap-1 '>{banner.rating}<IoStarHalfOutline/> </p>
-                    <p className=''>{banner.offer}</p>
-                  </div>
-                  <button className=''>{banner.buttonText}</button>
+              {/* Text content */}
+              <div className="flex flex-col gap-3 w-[75%] md:w-[40%] absolute top-1/2 left-4 sm:left-10  lg:top-2/3 transform -translate-y-1/2  text-white">
+                <div className="flex flex-col gap-2 sm:gap-4">
+                  <p className="text-3xl sm:text-4xl font-bold style2-font">
+                    {banner.title}
+                  </p>
+                  <p className="text-1xl font-thin sm:text-4xl">{banner.title2}</p>
+                  <p className="hidden md:block text-1xl leading-5 style-font">{banner.dec}</p>
+                </div>
+
+                <div className="flex items-center gap-3">
+                  <p className="flex items-center gap-1 ">
+                    {banner.rating}
+                    <IoStarHalfOutline />{" "}
+                  </p>
+                  <p className="">{banner.offer}</p>
+                </div>
+                <div>
+                <button className="bg-custom-orange text-white py-2 sm:py-3 px-4 sm:px-10 rounded-sm shadow-md hover:bg-white border-2 hover:border-custom-orange hover:text-black outline-none transition duration-300">{banner.buttonText}</button>
+                </div>
               </div>
             </div>
           </SwiperSlide>
--- cr-restaurant/tailwind.config.js
+++ cr-restaurant/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-orange': '#ad954f',
+      },
+    },
   },
   plugins: [],
 }
