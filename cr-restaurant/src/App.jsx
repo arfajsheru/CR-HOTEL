91ad6c2e50344caafa1ff1e19905117dff3754b6
arfajsheru <arfatsheru74@gmail.com>
2024-09-04 16:50:19 +0530

    added our policy

--- cr-restaurant/src/App.jsx
+++ cr-restaurant/src/App.jsx
@@ -1,36 +1,52 @@
-import React from 'react'
-import {Routes,Route} from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Menu from "./pages/Menu"
-import Contact from "./pages/Contact"
-import Cart from "./pages/Cart"
-import Order from "./pages/Order"
-import Login from "./pages/Login"
-import Product from "./pages/Product"
-import PlaceOrder from './PlaceOrder'
-import Navbar from "./component/Navbar"
-import Footer from './component/Footer'
-
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Order from "./pages/Order";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import PlaceOrder from './PlaceOrder';
+import Navbar from "./component/Navbar";
+import Footer from './component/Footer';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    
+    setTimeout(() => {
+      setLoading(false);
+    }, 9000); // 
+  }, []);
+
   return (
     <div className='px-4 sm:px-[1vw] md:px-[2vw] lg:px-[3vw] app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/menu' element={<Menu />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/cart' element={<Cart />}/>
-        <Route path='/order' element={<Order />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/place-order' element={<PlaceOrder />}/>
-        <Route path='/product/:id' element={<Product />}/>
-      </Routes>
-      <Footer />
+      {loading ? (
+        <div className='flex justify-center items-center h-screen'>
+          <span className='loader'></span>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/menu' element={<Menu />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/order' element={<Order />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/place-order' element={<PlaceOrder />} />
+            <Route path='/product/:id' element={<Product />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
